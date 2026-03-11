50dbc149f38cc41a7dbc2327d0246595eb6f945a
Keshav Rao Maratha <2024btechaidskeshav17932@poornima.edu.in>
2026-03-12 00:15:53 +0530

    Add files via upload

---
+++ welcome_generator.py
@@ -0,0 +1,8 @@
+name = input("What's your name? ")
+hobby = input("What's your favorite hobby? ")
+
+print("\n--- Welcome Message ---")
+print(f"Hello, {name}!")
+print(f"Welcome to the world of python programming.")
+print(f"It's great to know that you love {hobby}.")
+print(f"Get ready to ready build something amazing today.")
