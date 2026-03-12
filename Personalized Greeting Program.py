e4a74ab1865585afef66eb113c9e73183a4e403c
Keshav Rao Maratha <2024btechaidskeshav17932@poornima.edu.in>
2026-03-13 00:38:52 +0530

    Add files via upload

---
+++ Personalized Greeting Program.py
@@ -0,0 +1,8 @@
+name = input("What is your name? ")
+age = int(input("How old are you? "))
+color = input("What is your favorite color? ")
+
+print("\n---- Personalized Greeting ----")
+print(f"Hello, {name}!")
+print(f"You are {age} years old and {color} is a beautiful color!")
+print(f"You're now ready to start Python adventure")
